f04f614e3893911b39381f8ba498d7f1b7c48014
Darby Johnston <darbyjohnston@yahoo.com>
2025-09-04 17:24:57 -0700

    Make timeline ticks more visible
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- lib/toucanView/TimelineItem.cpp
+++ lib/toucanView/TimelineItem.cpp
@@ -457,9 +457,9 @@ namespace toucan
                         g.min.x +
                         t / duration * w,
                         _size.scrollPos.y +
-                        g.min.y +
-                        _size.fontMetrics.lineHeight,
+                        g.min.y,
                         _size.border,
+                        _size.fontMetrics.lineHeight +
                         _size.margin * 2);
                     if (intersects(box, drawRect))
                     {
@@ -517,7 +517,7 @@ namespace toucan
                 {
                     event.render->drawMesh(
                         mesh,
-                        event.style->getColorRole(ftk::ColorRole::Button));
+                        event.style->getColorRole(ftk::ColorRole::TextDisabled));
                 }
             }
         }
