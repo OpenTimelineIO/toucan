36ba4988c9541d5f1b95e46a7a34f4c2435242a9
Darby Johnston <darbyjohnston@yahoo.com>
2025-09-16 10:54:23 -0700

    Debugging
    Signed-off-by: Darby Johnston <darbyjohnston@yahoo.com>

--- cmake/SuperBuild/Buildexpat.cmake
+++ cmake/SuperBuild/Buildexpat.cmake
@@ -7,16 +7,14 @@ set(expat_ARGS
     ${toucan_EXTERNAL_PROJECT_ARGS}
     -DEXPAT_BUILD_TOOLS=OFF
     -DEXPAT_BUILD_EXAMPLES=OFF
-    -DEXPAT_BUILD_TESTS=OFF)
+    -DEXPAT_BUILD_TESTS=OFF
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5)
 
 ExternalProject_Add(
     expat
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/expat
     GIT_REPOSITORY ${expat_GIT_REPOSITORY}
     GIT_TAG ${expat_GIT_TAG}
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${CMAKE_CURRENT_SOURCE_DIR}/expat-patch/expat/CMakeLists.txt
-        ${CMAKE_CURRENT_BINARY_DIR}/expat/src/expat/expat/CMakeLists.txt
     SOURCE_SUBDIR expat
     LIST_SEPARATOR |
     CMAKE_ARGS ${expat_ARGS})
--- cmake/SuperBuild/Buildyaml-cpp.cmake
+++ cmake/SuperBuild/Buildyaml-cpp.cmake
@@ -7,15 +7,13 @@ set(yaml-cpp_ARGS
     ${toucan_EXTERNAL_PROJECT_ARGS}
     -DYAML_CPP_BUILD_CONTRIB=OFF
     -DYAML_CPP_BUILD_TOOLS=OFF
-    -DYAML_CPP_BUILD_TESTS=OFF)
+    -DYAML_CPP_BUILD_TESTS=OFF
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5)
 
 ExternalProject_Add(
     yaml-cpp
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/yaml-cpp
     GIT_REPOSITORY ${yaml-cpp_GIT_REPOSITORY}
     GIT_TAG ${yaml-cpp_GIT_TAG}
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        ${CMAKE_CURRENT_SOURCE_DIR}/yaml-cpp-patch/CMakeLists.txt
-        ${CMAKE_CURRENT_BINARY_DIR}/yaml-cpp/src/yaml-cpp/CMakeLists.txt
     LIST_SEPARATOR |
     CMAKE_ARGS ${yaml-cpp_ARGS})
